4470c74cbb372dac986783e554b0c2ab07ce478c
Negarm2025 <117588928+Negarm2025@users.noreply.github.com>
2022-11-26 18:28:56 +0330

    Add files via upload

---
+++ WeeeK7(2).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ main() {
+	int a,b,c,i,mod;
+	cout<<"Enter first number:";
+	cin>> a;
+	cout<<"Enter second number:";
+	cin >>b;
+	c=0;
+	i=0;
+	while (a-c>=b){
+		c=c+b;
+		mod=a-c;
+		i=i+1;
+	}
+	cout<<"baqimandeh:"<<mod<<"\n";
+	cout<<"kharej ghesmat:"<<i;
+}
